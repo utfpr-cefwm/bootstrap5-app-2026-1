91ccf8497db844598d0e90f546798b20608511ef
Rui Leite <ruipimentelleite@gmail.com>
2026-05-16 17:13:36 -0300

    "Conserta" CORS pontualmente para o GET /users.

--- server/routes/users.js
+++ server/routes/users.js
@@ -5,6 +5,7 @@ var router = express.Router();
 router.get('/', function(req, res, next) {
   const data = new Date();
   console.log(data);
+  res.setHeader("Access-Control-Allow-Origin", "http://localhost:8080");
   res.json({
     "Usuário": "rui",
     "Data Atual": data,
