03cf1b46c009c5a6559e1485d99c0e9f885d0d5c
Rui Leite <ruipimentelleite@gmail.com>
2026-05-16 11:21:42 -0300

    Mostra como fazer tratamento do  comportamento padrão do evento.

--- js/app.js
+++ js/app.js
@@ -1,5 +1,6 @@
 const form = document.querySelector("#form-cor");
-form.addEventListener("submit", function() {
+form.addEventListener("submit", function(event) {
+  event.preventDefault();
 
   const caixa = document.querySelector("#input-cor");
   document.body.style.backgroundColor = caixa.value;
