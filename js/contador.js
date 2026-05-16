1cb512247d0f4782665286a2780f01b589e9c1af
Rui Leite <ruipimentelleite@gmail.com>
2026-05-16 13:43:40 -0300

    Contador (com mudança de cor de texto).

--- js/contador.js
+++ js/contador.js
@@ -2,9 +2,29 @@ const botaoMais = document.querySelector("#botao-mais");
 const botaoMenos = document.querySelector("#botao-menos");
 const output = document.querySelector("output");
 
+function alertaCor(atual) {
+    resetCor()
+    if (atual > 0){
+        output.classList.add('text-success')
+    }
+    if (atual < 0){
+        output.classList.add('text-danger')
+    }
+}
+
+function resetCor(){
+    output.classList.remove('text-success')
+    output.classList.remove('text-danger')
+}
+
 function fnTratadora(dif) {
   const valor = +output.innerHTML;  // Converte de string para number.
   output.innerHTML = valor + dif;
+
+  alertaCor(valor + dif);
+
+  // output.classList.replace("minha-classe-1", "minha-classe-2");
+
 }
 
 botaoMais.addEventListener("click", () => fnTratadora(+1) );
